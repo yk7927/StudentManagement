2423b021572bb7ec26a8b4a83504b2effb8b4b51
yukangkim <yugang7927@gmail.com>
2026-08-03 17:45:36 +0900

    feat: enhance login logic

--- login.js
+++ login.js
@@ -1,1 +1,9 @@
-console.log("login") 
+function login(username, password) {
+    if (username === "admin" && password === "1234") {
+        console.log("로그인 성공!");
+        return true;
+    } else {
+        console.log("로그인 실패!");
+        return false;
+    }
+}
